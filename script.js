1ba5902f006756ca75101f2f07f704ec34b04ea4
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-09-07 21:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let n1=0,n2=1,n3;
+	
+	for(let i=2;i<num;i++){
+		n3=n1+n2;
+		
+		n1=n2;
+		n2=n1;
+		return n3;
+	}
 }
-
+fibonacci(5)
 module.exports = fibonacci;
